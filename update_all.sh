efc77df6f6f1410b0e416b03bf3f4d1e8dcfad55
Leon Reinschmidt <4107480+LeonRein@users.noreply.github.com>
2024-04-06 16:30:15 +0200

    added git pull to update all

--- update_all.sh
+++ update_all.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
+systemctl stop klipper.service
+
+pushd ~/klipper
+git pull
+pulld
+
+pushd ~/katapult
+git pull
+pulld
+
 ./update_octopus.sh
 ./update_ebb.sh
+
+systemctl start klipper.service
